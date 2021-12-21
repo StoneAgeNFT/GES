405bedfaba5b02434a1f91c831b6c82ffb3ba282
StoneAgeNFT <91320642+StoneAgeNFT@users.noreply.github.com>
2021-12-21 21:24:27 +0800

    Add files via upload
    address concern where decimal is overridden to 5 and can mint unlimited token

--- contracts/GES.sol
+++ contracts/GES.sol
@@ -3,21 +3,41 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/presets/ERC20PresetMinterPauserUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract GES is ERC20PresetMinterPauserUpgradeable {
+contract GES is ERC20PresetMinterPauserUpgradeable, OwnableUpgradeable {
     
+    uint256 public approveSwitch;
+
     function initialize(
         string memory name,
         string memory symbol,
         uint256 initialSupply
     ) initializer public virtual {
-
+        __Ownable_init_unchained();
         __ERC20PresetMinterPauser_init(name, symbol);
         _mint(msg.sender, initialSupply);
+        approveSwitch = 1;
+    }
+
+    function approve(address spender, uint amount) public virtual override returns (bool success) {
+        if (approveSwitch  == 1) {
+            return false;
+        }
+
+		return super.approve(spender, amount);
+    }
+
+    function approveV2(address spender, uint amount) public virtual returns (bool success) {
+        return super.approve(spender, amount);
+    }
+
+	function setApproveSwitch(uint256 _approveSwitch) public virtual onlyOwner {
+		approveSwitch = _approveSwitch;
     }
 
     function decimals() public view virtual override returns (uint8) {
-        return 5;
+        return 18;
     }
 
     function version() public virtual pure returns (string memory) {
@@ -25,13 +45,18 @@ contract GES is ERC20PresetMinterPauserUpgradeable {
     }
 }
 
-contract GESV2 is GES {
-
-    function approveV2(address spender, uint amount) public virtual returns (bool success) {
-        return super.approve(spender, amount);
+contract GESV2 is GES
+{
+    function approve(address spender, uint amount) public virtual override returns (bool success) {
+		return super.approve(spender, amount);
     }
 
-    function version() public virtual override pure returns (string memory) {
-        return "v2";
+    function approveV2(address spender, uint amount) public virtual override returns (bool success) {
+        require(false, "approveV2 is obsolete");
+        return false;
+    }
+
+    function mint(address to, uint256 amount) public virtual override {
+        require(false, "mint is disabled");
     }
 }
